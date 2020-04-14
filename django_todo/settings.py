52a961e66bd3eeeaf29cb7c609c1a91b6424be0e
Msmiles <muhammadirfan123@hotmail.com>
2020-04-14 15:14:35 +0000

    Update ALLOWED_HOSTS

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'k9v9yv%hd&#r_xcjgtpa-g8138h8*@v#0_03x%um3ffob4t1ll'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-a213c930-a559-4432-89bf-5ce95d72d557.ws-eu01.gitpod.io']
+ALLOWED_HOSTS = ['8000-a213c930-a559-4432-89bf-5ce95d72d557.ws-eu01.gitpod.io',
+                 'new-djando-todo.herokuapp.com']
 
 
 # Application definition
